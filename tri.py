8ab120e8db3d06b571939b9c0e9652a815c6f2d8
NARENDRAN_ <narend925@gmail.com>
2021-11-26 23:27:28 +0530

    loop enumerate

--- tri.py
+++ tri.py
@@ -1,4 +1,10 @@
 # user input for length of prim
-PRIM_LEN = int(input("Enter the length : "))
-for tmp in range(PRIM_LEN):
-    print("*"*tmp)
+# PRIM_LEN = int(input("Enter the length : "))
+# for tmp in range(PRIM_LEN):
+#     print("*"*tmp)
+
+
+l1 = ["a","b","c","d"]
+
+for num , tmp in enumerate(l1):
+    print("{} : {}".format(num,tmp))
